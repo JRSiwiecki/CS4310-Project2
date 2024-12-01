c487845fa90ec08e48b29b607e8ca4c9a6666fa8
JRSiwiecki <JRSiwiecki@gmail.com>
2024-12-01 13:15:51 -0800

    Change output to look clearer

--- algorithms/fifo.py
+++ algorithms/fifo.py
@@ -5,9 +5,9 @@ def run_algorithm(reference_string, frame_size):
     print(reference_string)
 
     for page in reference_string:
-
         if page in page_frame:
             fault_track += " "
+            print(page_frame)
             continue
 
         if len(page_frame) >= frame_size:
@@ -16,10 +16,13 @@ def run_algorithm(reference_string, frame_size):
         page_frame.append(page)
         fault_track += "X"
 
+        print(page_frame, end=" ")
+        if fault_track[-1] == "X":
+            print("X")
+
     fault_count = 0
     for char in fault_track:
         if char == "X":
             fault_count += 1
 
-    print(fault_track)
-    print("FIFO yields", fault_count, "faults with frame size", frame_size)
+    print("FIFO yields", fault_count, "page faults with frame size", frame_size)
