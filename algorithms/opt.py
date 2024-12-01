0dbe766bc2c5b1ec29de25a174201fd442e3df23
JRSiwiecki <JRSiwiecki@gmail.com>
2024-12-01 13:51:51 -0800

    Fix OPT bug

--- algorithms/opt.py
+++ algorithms/opt.py
@@ -1,7 +1,6 @@
 def run_algorithm(reference_string, frame_size):
     page_frame = []
     fault_track = ""
-
     print("Running OPT on", reference_string, "with frame size", frame_size)
 
     for current_index, page in enumerate(reference_string):
@@ -14,30 +13,35 @@ def run_algorithm(reference_string, frame_size):
 
         fault_track += "X"
 
-        # If the frame is full, replace the optimal page
-        if len(page_frame) >= frame_size:
-            furthest_usage = -1
-            page_to_replace = None
+        # If the frame is not full, add the page
+        if len(page_frame) < frame_size:
+            page_frame.append(page)
+            print(page, "-", page_frame, "X")
+            continue
 
-            for frame_page in page_frame:
-                # Find the next occurrence of the current frame page
-                if frame_page in reference_string[current_index + 1 :]:
-                    next_usage = reference_string[current_index + 1 :].index(frame_page)
-                else:
-                    next_usage = float("inf")  # Page won't be used again
+        # Frame is full, find the page to replace
+        page_to_replace = None
+        max_future_distance = -1
 
-                # Select the page with the furthest future usage
-                if next_usage > furthest_usage:
-                    furthest_usage = next_usage
-                    page_to_replace = frame_page
+        for frame_page in page_frame:
+            # Find the next occurrence of the frame page
+            try:
+                future_distance = reference_string[current_index + 1 :].index(
+                    frame_page
+                )
+            except ValueError:
+                # If the page is not found in future references, replace it
+                page_to_replace = frame_page
+                break
 
-            page_frame.remove(page_to_replace)
+            # Update page to replace if this page will be used furthest in the future
+            if future_distance > max_future_distance:
+                max_future_distance = future_distance
+                page_to_replace = frame_page
 
-        page_frame.append(page)
-
-        print(page, "-", page_frame, end=" ")
-        if fault_track[-1] == "X":
-            print("X")
+        # Replace the identified page
+        page_frame[page_frame.index(page_to_replace)] = page
+        print(page, "-", page_frame, "X")
 
     fault_count = fault_track.count("X")
     print("OPT yields", fault_count, "page faults")
