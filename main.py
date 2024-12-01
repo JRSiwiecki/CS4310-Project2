1b82ef83c0f7b22fdaa99c1b367f28d1f9c59177
JRSiwiecki <JRSiwiecki@gmail.com>
2024-12-01 14:12:09 -0800

    Fix misunderstanding with testing data, update main to match

--- main.py
+++ main.py
@@ -6,41 +6,31 @@ FRAME_SIZES = [3, 4, 5, 6]
 
 
 def testing_data_to_array():
-    testing_data_list = []
+    with open(TESTING_DATA_PATH + "TestingData.txt", "r", encoding="utf-8") as file:
+        string_list = []
 
-    for i in range(3):
-        with open(
-            TESTING_DATA_PATH + "TestingData-" + str(i) + ".txt", "r", encoding="utf-8"
-        ) as file:
-            string_list = []
+        for line in file.readlines():
+            string_list.append(line.strip())
 
-            for line in file.readlines():
-                string_list.append(line.strip())
-
-            testing_data_list.append(string_list)
-
-    return testing_data_list
+        return string_list
 
 
 def run_fifo_tests():
-    for testing_data in testing_data_to_array():
-        for reference_string in testing_data:
-            for frame_size in FRAME_SIZES:
-                fifo.run_algorithm(reference_string, frame_size)
+    for reference_string in testing_data_to_array():
+        for frame_size in FRAME_SIZES:
+            fifo.run_algorithm(reference_string, frame_size)
 
 
 def run_lru_tests():
-    for testing_data in testing_data_to_array():
-        for reference_string in testing_data:
-            for frame_size in FRAME_SIZES:
-                lru.run_algorithm(reference_string, frame_size)
+    for reference_string in testing_data_to_array():
+        for frame_size in FRAME_SIZES:
+            lru.run_algorithm(reference_string, frame_size)
 
 
-def run_opt_test():
-    for testing_data in testing_data_to_array():
-        for reference_string in testing_data:
-            for frame_size in FRAME_SIZES:
-                opt.run_algorithm(reference_string, frame_size)
+def run_opt_tests():
+    for reference_string in testing_data_to_array():
+        for frame_size in FRAME_SIZES:
+            opt.run_algorithm(reference_string, frame_size)
 
 
 def main():
