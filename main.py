7da01eb9ebf7b6e2015d3dccffab627a569369de
JRSiwiecki <JRSiwiecki@gmail.com>
2024-12-01 12:22:03 -0800

    Add testing_data_to_array function

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+TESTING_DATA_PATH = "testing-data/"
+
+
+def testing_data_to_array():
+    testing_data_list = []
+
+    for i in range(3):
+        with open(
+            TESTING_DATA_PATH + "TestingData-" + str(i) + ".txt", "r", encoding="utf-8"
+        ) as file:
+            string_list = []
+
+            for line in file.readlines():
+                string_list.append(line.strip())
+
+            testing_data_list.append(string_list)
+
+    return testing_data_list
+
+
 def main():
-    pass
+    print(testing_data_to_array())
 
 
 if __name__ == "__main__":
