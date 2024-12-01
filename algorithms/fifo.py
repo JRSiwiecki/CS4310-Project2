de72b0034fe51936e9ca6721c7c1939bd36585d6
JRSiwiecki <JRSiwiecki@gmail.com>
2024-12-01 13:05:41 -0800

    Initial FIFO algorithm

--- algorithms/fifo.py
+++ algorithms/fifo.py
@@ -1,2 +1,25 @@
-def run_algorithm():
-    pass
+def run_algorithm(reference_string, frame_size):
+    page_frame = []
+    fault_track = ""
+
+    print(reference_string)
+
+    for page in reference_string:
+
+        if page in page_frame:
+            fault_track += " "
+            continue
+
+        if len(page_frame) >= frame_size:
+            page_frame.pop(0)
+
+        page_frame.append(page)
+        fault_track += "X"
+
+    fault_count = 0
+    for char in fault_track:
+        if char == "X":
+            fault_count += 1
+
+    print(fault_track)
+    print("FIFO yields", fault_count, "faults")
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from algorithms import fifo
+
 TESTING_DATA_PATH = "testing-data/"
 
 
@@ -19,7 +21,9 @@ def testing_data_to_array():
 
 
 def main():
-    print(testing_data_to_array())
+    for testing_data in testing_data_to_array():
+        for reference_string in testing_data:
+            fifo.run_algorithm(reference_string, 3)
 
 
 if __name__ == "__main__":
