ef8f243062f76f4926761455de75d61b18991e17
JRSiwiecki <JRSiwiecki@gmail.com>
2024-12-01 13:42:17 -0800

    Make FIFO nicer

--- algorithms/fifo.py
+++ algorithms/fifo.py
@@ -2,27 +2,30 @@ def run_algorithm(reference_string, frame_size):
     page_frame = []
     fault_track = ""
 
-    print(reference_string)
+    print("Running FIFO on", reference_string, "with frame size", frame_size)
 
     for page in reference_string:
+
+        # If page already in page_frame, no need to change anything
         if page in page_frame:
             fault_track += " "
-            print(page_frame)
+            print(page, "-", page_frame)
             continue
 
+        # Otherwise, if page_frame is already full, need to remove
+        # the current first element in the frame
         if len(page_frame) >= frame_size:
             page_frame.pop(0)
 
+        # Then we can add our new page
         page_frame.append(page)
         fault_track += "X"
 
-        print(page_frame, end=" ")
+        print(page, "-", page_frame, end=" ")
+
         if fault_track[-1] == "X":
             print("X")
 
-    fault_count = 0
-    for char in fault_track:
-        if char == "X":
-            fault_count += 1
+    fault_count = fault_track.count("X")
 
-    print("FIFO yields", fault_count, "page faults with frame size", frame_size)
+    print("FIFO yields", fault_count, "page faults")
